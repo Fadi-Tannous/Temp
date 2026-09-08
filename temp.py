7e0e732d83877a6ca2931fda7a5817e17e0d92d7
Fadi-Tannous <fadi.tann@gmail.com>
2026-09-08 19:36:03 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,1 @@
-LIST BOUNTIES 200
-200
-b'{"bounty_id": "e5bb0a4d-ee57-4d57-882e-46aa0557db65", "purespectrum_user_survey_id": "514165d1-44e9-4e59-866c-06611014b9f6", "entry_link": "https://fusion.spectrumsurveys.com/start-session/649US0038?langIsoCode=en&surveyId=52122750&ts=1788885291215&maxLoi=6&matchApiId=Eaiyq4r042L3x4xMSJhIJ&211=111&212=34&229=32789&sPlacement=1&surveySig=d4bc6b906640bc0d2b10fe49e9d1ca977ffa54612076436d03d8f458d7399d92&f_hash=4f83ab77d54ae94ac8a5213c46400e656815aac4"}'
+
